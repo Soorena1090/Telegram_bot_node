566218d491c52fcbf64ff4029bb9fe4b175e1d3b
Soorena Mahbooby <soorenamahbobi@gmail.com>
2025-05-12 16:04:09 +0330

    install dotenv and extend dotenv to config.js

--- config/config.js
+++ config/config.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 const DATABASE_URL = process.env.DATABASE_URL;
 const TELEGRAM_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 
---
+++ index.js
@@ -0,0 +1,3 @@
+import { DATABASE_URL } from "./config/config.js";
+
+console.log(DATABASE_URL);
