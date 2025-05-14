66be65f016169aae4a7fe56b2feb163d6f38daa5
Soorena Mahbooby <soorenamahbobi@gmail.com>
2025-05-14 18:14:59 +0330

    m

--- index.js
+++ index.js
@@ -5,18 +5,3 @@
 // formatMessage();
 // // fetchRssFeeds();
 // parsFeedsAndFetchData();
-import {TELEGRAM_TOKEN} from './config/config.js'
-import TelegramBot from 'node-telegram-bot-api';
-
-// توکن باتت رو اینجا بذار
-const token = TELEGRAM_TOKEN;
-
-// ساخت بات در حالت polling
-const bot = new TelegramBot(token, { polling: true });
-
-// وقتی پیامی دریافت شد
-bot.on('message', (msg) => {
-  console.log('📥 New message received!');
-  console.log('👤 From:', msg.chat.title || msg.chat.username || msg.chat.first_name);
-  console.log('🆔 Chat ID:', msg.chat.id);
-});
